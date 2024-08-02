8ef385c0a66ae59cd22971e3a6c3c17305546bc6
Théo FRERE <theo.frere@umanis.com>
2024-08-02 16:11:16 +0200

    pre update angular

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyCk2pqJq5Vm7vOhz4kH_YRIuRjkOC6K-KM",
+        authDomain: "waldotours-42416.firebaseapp.com",
+        databaseURL: "https://waldotours-42416-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "waldotours-42416",
+        storageBucket: "waldotours-42416.appspot.com",
+        messagingSenderId: "792793844716",
+        appId: "1:792793844716:web:2cf7451dcf283c4a818ec9"
+    }
+  };
